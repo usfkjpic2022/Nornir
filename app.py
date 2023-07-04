66fa7ba3013ef474b3c48a59bf8427202544b7c4
irack000 <IRACK000@gmail.com>
2023-07-04 17:14:58 +0900

    Fix: Fix Orign Error

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ class CachingRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Cache-Control', 'max-age=315360000')  # 캐싱을 10년으로 설정
         self.send_header('Expires', time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime(time.time() + 315360000)))  # 만료 시간 설정
+        self.send_header('Access-Control-Allow-Origin', '*')  # 캐싱을 10년으로 설정
         super().end_headers()
 
 # 서버 실행
